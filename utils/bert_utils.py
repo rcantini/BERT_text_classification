c26e99cee3b90fea69389adab165ae454c37463a
Riccardo Cantini <68375719+rcantini@users.noreply.github.com>
2024-03-23 00:05:41 +0100

    Update bert_utils.py

--- utils/bert_utils.py
+++ utils/bert_utils.py
@@ -6,6 +6,7 @@ def prepare_bert_input(sentences, seq_len, bert_name):
     tokenizer = BertTokenizer.from_pretrained(bert_name)
     encodings = tokenizer(sentences.tolist(), truncation=True, padding='max_length',
                                 max_length=seq_len)
-    input = [np.array(encodings["input_ids"]), np.array(encodings["token_type_ids"]),
-               np.array(encodings["attention_mask"])]
+    input = [np.array(encodings["input_ids"]),
+             np.array(encodings["attention_mask"]),
+             np.array(encodings["token_type_ids"])]
     return input
